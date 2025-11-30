a99deef8ddd2992f35aa68fe2a016f65c66abe89
David Reis <davidsilvareis23@gmail.com>
2025-11-30 18:30:45 -0300

    Removendo pastas listas do rastreamento do GitHub

--- lista-01/questao-2-a.js
+++
@@ -1,6 +0,0 @@
-let reais = 5;
-let cotacaoDolar = 5.4;
-
-let conversao = reais / cotacaoDolar;
-
-console.log("O resultado da conversão é: US$ " + conversao.toFixed(2));
--- lista-01/questao-3-e.js
+++
@@ -1,9 +0,0 @@
-function calcularTotal(valorUnitario, quantidade, desconto = 0) {
-  let total = valorUnitario * quantidade;
-  total -= total * desconto;
-  return total;
-}
-
-console.log("\ne) Valor total da compra:");
-console.log(calcularTotal(100, 2))
-console.log(calcularTotal(100, 2, 0.1));
